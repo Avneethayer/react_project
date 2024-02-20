3672fb1f10baeb58b32734cfe6b3e64c5e2406cc
Avneet Kaur <avneet.hayer1@gmail.com>
2024-02-20 15:55:21 -0600

    change color conditionally

--- 02-steps/src/App.js
+++ 02-steps/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css"
 
 function App() {
+  const step = 3
   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
       <div className="content">Step 1: Learn React</div>
       <div className="buttons">
