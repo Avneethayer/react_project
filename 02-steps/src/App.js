4c0613a8fa4cd84ef77cefbb4a98d3c58409f8c8
Avneet Kaur <avneet.hayer1@gmail.com>
2024-02-20 16:33:11 -0600

    using states

--- 02-steps/src/App.js
+++ 02-steps/src/App.js
@@ -1,18 +1,30 @@
 import "./App.css"
+import { useState } from "react"
+
+const messages = ["Learn HTML, CSS, JavaScript", "Learn React", "Learn NodeJS"]
 
 function App() {
-  const step = 3
+  const [step, setStep] = useState(1)
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1)
+  }
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1)
+  }
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <div className="content">Step 1: Learn React</div>
+      <div className="content">
+        Step {step}: {messages[step - 1]}
+      </div>
       <div className="buttons">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
       </div>
     </div>
   )
