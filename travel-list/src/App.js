7c7da435376a5578467cc6d837009f6654277d4a
Avneet Kaur <avneet.hayer1@gmail.com>
2024-03-04 17:01:12 -0600

    lifting state up to update packed property in items

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react"
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 10, packed: true },
-  { id: 3, description: "Shoes", quantity: 12, packed: false },
-]
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 10, packed: true },
+//   { id: 3, description: "Shoes", quantity: 12, packed: false },
+// ]
 
 export default function App() {
   const [items, setItems] = useState([])
@@ -14,14 +14,26 @@ export default function App() {
   }
 
   function handleDeleteItems(id) {
-    setItems((items) => items.filter((items) => items.id !== id))
+    setItems((items) => items.filter((item) => item.id !== id))
+  }
+
+  function handleToggleItems(id) {
+    setItems((items) =>
+      items.map(
+        (item) => (item.id = id ? { ...item, packed: !item.packed } : item)
+      )
+    )
   }
 
   return (
     <div className="App">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItems={handleDeleteItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItems}
+        onToggleItems={handleToggleItems}
+      />
       <Stats />
     </div>
   )
@@ -78,23 +90,33 @@ function Form({ onAddItems }) {
   )
 }
 
-function PackingList({ items, onDeleteItems }) {
+function PackingList({ items, onDeleteItems, onToggleItems }) {
   return (
     <div className="list">
       <h2 className="list_heading">List</h2>
       <ul className="list_item">
         {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItems={onDeleteItems} />
+          <Item
+            item={item}
+            key={item.id}
+            onDeleteItems={onDeleteItems}
+            onToggleItems={onToggleItems}
+          />
         ))}
       </ul>
     </div>
   )
 }
 
-function Item({ item, onDeleteItems }) {
+function Item({ item, onDeleteItems, onToggleItems }) {
   return (
     <li>
       <div className={!item.packed ? "not_packed" : ""}>
+        <input
+          type="checkbox"
+          value={item.packed}
+          onChange={() => onToggleItems(item.id)}
+        />
         {item.description}
         {item.quantity}
         <button onClick={() => onDeleteItems(item.id)}>❌</button>
