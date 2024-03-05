05b8bc8cdadf728eae0eef1f97dd7b174b257b30
Avneet Kaur <avneet.hayer1@gmail.com>
2024-03-04 18:28:29 -0600

    sorting the items based on conditions

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -90,11 +90,24 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItems, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input")
+
+  let sortedItems
+
+  if (sortBy === "input") sortedItems = items
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description))
+
+  if (sortBy === "packingOrder")
+    sortedItems = items.slice().sort((a, b) => a.packed - b.packed)
+
   return (
     <div className="list">
       <h2 className="list_heading">List</h2>
       <ul className="list_item">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -103,6 +116,11 @@ function PackingList({ items, onDeleteItems, onToggleItems }) {
           />
         ))}
       </ul>
+      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="description">Sort by description</option>
+        <option value="packingOrder">Sort by packing order</option>
+      </select>
     </div>
   )
 }
