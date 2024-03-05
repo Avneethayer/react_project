6683c2def1e187d2f3f87879928c3292b6c0592a
Avneet Kaur <avneet.hayer1@gmail.com>
2024-03-04 18:58:08 -0600

    finished project

--- travel-list/src/components/App.js
+++ travel-list/src/components/App.js
@@ -1,14 +1,8 @@
 import { useState } from "react"
 import Logo from "./Logo"
 import PackingList from "./PackingList"
-
 import Form from "./Form"
 import Stats from "./Stats"
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 10, packed: true },
-//   { id: 3, description: "Shoes", quantity: 12, packed: false },
-// ]
 
 export default function App() {
   const [items, setItems] = useState([])
